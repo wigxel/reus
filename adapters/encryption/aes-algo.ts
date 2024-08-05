931526a4b08d7975c80202693ee1a7b631804fa8
Joseph Owonvwon <joseph.owonwo@gmail.com>
2024-08-05 02:07:16 +0100

    chore: Update failing test case for Reversible algorithm

--- adapters/encryption/aes-algo.ts
+++ adapters/encryption/aes-algo.ts
@@ -1,4 +1,4 @@
-import { Config, Effect, Layer, pipe } from "effect";
+import { Config, Console, Effect, Layer, pipe } from "effect";
 import crypto from "uncrypto";
 import { arrayBufferToBase64, base64ToArrayBuffer } from "~/adapters/utils";
 import { HashingError, ReversibleHash } from "~/layers/encryption/reversible";
@@ -27,6 +27,7 @@ export class AESAlgo {
   static importKey(jwk: JsonWebKey) {
     return crypto.subtle.importKey("jwk", jwk, AESAlgo.algorithm, false, [
       "decrypt",
+      "encrypt",
     ]);
   }
 
@@ -48,7 +49,7 @@ export class AESAlgo {
   async encrypt(_message: string): Promise<ArrayBuffer> {
     // Set the plaintext message to encrypt
     const encodedMessage = new TextEncoder().encode(_message);
-
+    console.log(">>>", this.iv, this.key);
     // Encrypt message with IV and algorithm
     return await crypto.subtle.encrypt(
       { name: AESAlgo.algorithm.name, iv: this.iv },
@@ -73,47 +74,68 @@ export class AESAlgo {
   }
 }
 
-export function createReversibleHash() {
-  const getCryptoKey = (jwk: JsonWebKey) =>
-    Effect.tryPromise({
-      try: () => AESAlgo.importKey(jwk),
-      catch: () => new HashingError("Error importing Crypto Key"),
-    });
-
-  const convertIV = (iv: string) =>
-    Effect.try({
-      try: () => base64ToArrayBuffer(iv),
-      catch: () => new HashingError("Error convert IV"),
-    });
-
-  const reversible = Effect.gen(function* (_) {
-    const jwk = yield* Config.string("ENCRYPTION_JWK").pipe(
-      Effect.flatMap((str) => JSON.parse(str)),
-      Effect.map((e) => e as JsonWebKey),
-    );
-    const iv_string = yield* Config.string("ENCRYPTION_IV");
-
-    const iv = yield* convertIV(iv_string);
-    const cryptoKey = yield* getCryptoKey(jwk);
-    const hash = new AESAlgo(cryptoKey, iv);
-
-    return {
-      encrypt: (value: string) =>
-        pipe(
-          Effect.tryPromise(() => hash.encrypt(value)),
-          Effect.map((buffer) => arrayBufferToBase64(buffer)),
-          Effect.mapError(() => new HashingError("Error encrypting content")),
-        ),
-      decrypt: (value: string) =>
-        pipe(
-          Effect.tryPromise(() => hash.decrypt(base64ToArrayBuffer(value))),
-          Effect.mapError(() => new HashingError("Error decrypting content")),
-        ),
-    };
+const getCryptoKey = (jwk: JsonWebKey) => {
+  return Effect.tryPromise({
+    try: () => AESAlgo.importKey(jwk),
+    catch: () => new HashingError("Error importing Crypto Key"),
   });
+};
 
-  return Layer.effect(ReversibleHash, reversible);
-}
+const convertIV = (iv: string) => {
+  return Effect.try({
+    try: () => {
+      return Uint8Array.from(iv.split(","), (e) => Number(e));
+    },
+    catch: () => new HashingError("Error convert IV"),
+  });
+};
+
+const getEncryptionCred = Effect.gen(function* () {
+  const jwk_string = yield* Config.string("ENCRYPTION_JWK");
+  const iv_string = yield* Config.string("ENCRYPTION_IV");
+
+  const jwk_parsed = yield* pipe(
+    Effect.try({
+      try: () => JSON.parse(jwk_string),
+      catch: () => new HashingError("Error parsing JWK string"),
+    }),
+    Effect.map((e) => e as JsonWebKey),
+  );
+
+  const iv = yield* convertIV(iv_string);
+  const cryptoKey = yield* getCryptoKey(jwk_parsed);
+
+  yield* Console.log("Parsed Key:", { iv, jwk_parsed });
+
+  return { iv, cryptoKey };
+});
+
+const reversible = Effect.gen(function* (_) {
+  const { cryptoKey, iv } = yield* getEncryptionCred;
+  const hash = new AESAlgo(cryptoKey, iv);
+
+  const encrypt = (value: string) => {
+    return Effect.tryPromise({
+      try: () => hash.encrypt(value),
+      catch: (err) => {
+        return new HashingError(`EncryptionError: ${String(err)}`);
+      },
+    }).pipe(Effect.map((buffer) => arrayBufferToBase64(buffer)));
+  };
+
+  const decrypt = (value: string) =>
+    pipe(
+      Effect.tryPromise(() => hash.decrypt(base64ToArrayBuffer(value))),
+      Effect.mapError(() => new HashingError("Error decrypting content")),
+    );
+
+  return {
+    encrypt,
+    decrypt,
+  };
+});
+
+export const createReversibleHash = Layer.effect(ReversibleHash, reversible);
 
 export async function generateSecrets() {
   const crypto = await AESAlgo.generateKey();
@@ -121,6 +143,6 @@ export async function generateSecrets() {
 
   return {
     ENCRYPTION_JWK: await AESAlgo.exportKey(crypto),
-    ENCRYPTION_IV: iv,
+    ENCRYPTION_IV: String(iv),
   };
 }
--- adapters/encryption/time-based.test.ts
+++ adapters/encryption/time-based.test.ts
@@ -2,19 +2,16 @@ import { ConfigProvider, Effect, Layer } from "effect";
 import { createReversibleHash } from "~/adapters/encryption/aes-algo";
 import { TimeBasedToken } from "~/adapters/encryption/time-based";
 
-describe("Time based token", () => {
-  it("should add reversible time-based token", () => {
-    const SALT = "hello";
-    const IV_HEX = "c28d1b0086ac1d7c82d57ac22d5abbaf";
+const config = ConfigProvider.fromJson({
+  ENCRYPTION_JWK: `{"key_ops":["decrypt","encrypt"],"ext":true,"kty":"oct","k":"swnbYyH2_3LohW0Bro4Ytg","alg":"A128GCM"}`,
+  ENCRYPTION_IV: "41,129,192,219,14,151,82,23,218,152,92,218",
+});
 
-    const deps = Layer.merge(
-      createReversibleHash({ hex: "", salt: "" }),
-      Layer.setConfigProvider(
-        ConfigProvider.fromJson({
-          HASHING_MAILING_SALT: SALT,
-          HASHING_IV_HEX: IV_HEX,
-        }),
-      ),
+describe("Time based token", () => {
+  it("should add reversible time-based token", async () => {
+    const deps = Layer.empty.pipe(
+      Layer.provideMerge(createReversibleHash),
+      Layer.provideMerge(Layer.setConfigProvider(config)),
     );
 
     const effect = TimeBasedToken.encrypt(
@@ -22,11 +19,12 @@ describe("Time based token", () => {
       new Date(2024, 6, 26),
     );
 
-    const token = Effect.runSync(Effect.provide(effect, deps));
+    const token = Effect.runPromise(Effect.provide(effect, deps));
 
     // Usage as a token for email links
     // `https://localhost:3000/customer/events?userId=12&token=${token}`
-    //
-    expect(typeof token === "string").toBe(true);
+    expect(token).resolves.toMatchInlineSnapshot(
+      `"n3hgQCfJs6o9Vam9RKv1dqNckPDNJkStkJjNNLPIJstpkqi2hG97/nI8o1n3duvOWpCcBKQ2V3fBMCyNyW8MofGzAW7EGA4="`,
+    );
   });
 });
